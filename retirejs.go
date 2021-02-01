8ee8100bf4aeb3c350c0227808fb14da940adb17
Erik van Oosbree <erikvanoosbree@hotmail.com>
2021-02-01 23:56:01 +0100

    Made it possible to check if domain name is registed

--- retirejs.go
+++ retirejs.go
@@ -3,15 +3,33 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"github.com/haccer/available"
+	"strings"
 )
 
-func getSrcLinks(htmlData []byte){
+func getSrcLinks(htmlData []byte, baseurl string){
 	scriptExp := regexp.MustCompile(`<script[^>]+\bsrc=["']([^"']+)["']`)
 
 	scriptMatchSlice := scriptExp.FindAllStringSubmatch(string(htmlData), -1)
 
 	for _, item := range scriptMatchSlice {
-		fmt.Println("Script SRC found : ", item[1])
+		if strings.Contains(item[1], string("http")) == true{
+			fmt.Println("Script SRC found : ", item[1])
+
+			checkDomainAvailable(item[1])
+		}else{
+			fmt.Println("Script SRC found : ", baseurl+item[1])
+			checkDomainAvailable(baseurl+item[1])
+		}
+	}
+}
+
+func checkDomainAvailable(domainname string){
+	available := available.Domain(domainname)
+	if available {
+		fmt.Println("[+] Domain not registed!")
+	}else{
+		fmt.Println("Domain is registed")
 	}
 }
 
