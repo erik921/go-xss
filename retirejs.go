9e2c73c7884377c6e029e19a978fa007f48819f3
Erik van Oosbree <erikvanoosbree@hotmail.com>
2021-02-01 23:18:41 +0100

    changed file so I can have my program with multiple files

--- retirejs.go
+++ retirejs.go
@@ -5,10 +5,6 @@ import (
 	"regexp"
 )
 
-func ScanSignature(url string) () {
-	return
-}
-
 func getSrcLinks(htmlData []byte){
 	scriptExp := regexp.MustCompile(`<script[^>]+`)
 	scriptMatchSlice := scriptExp.FindAllStringSubmatch(string(htmlData), -1)
@@ -16,4 +12,8 @@ func getSrcLinks(htmlData []byte){
 	for _, item := range scriptMatchSlice {
 		fmt.Println("Script SRC found : ", item)
 	}
+}
+
+func ScanSignature(url string) () {
+	return
 }
