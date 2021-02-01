1e9021c45c410a06c6bf51bf1ec3612a7482d774
Erik van Oosbree <erikvanoosbree@hotmail.com>
2021-02-01 22:43:34 +0100

    Moved function

--- retirejs/retirejs.go
+++ retirejs/retirejs.go
@@ -1,6 +1,19 @@
 package retirejs
 
+import (
+	"fmt"
+	"regexp"
+)
 
-func retirejs(url string) () {
+func scanSignature(url string) () {
 	return
+}
+
+func getSrcLinks(htmlData []byte){
+	scriptExp := regexp.MustCompile(`<script[^>]+`)
+	scriptMatchSlice := scriptExp.FindAllStringSubmatch(string(htmlData), -1)
+
+	for _, item := range scriptMatchSlice {
+		fmt.Println("Script SRC found : ", item)
+	}
 }
